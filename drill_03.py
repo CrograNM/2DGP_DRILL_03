b909bdc24220ac80eb014991a75d8ee66cf16fc2
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2024-09-24 12:32:01 +0900

    삼각이동을 추가할 것.
    사각 이동과 마찬가지로 시작과 끝을 가운데로 지정하자

--- drill_03.py
+++ drill_03.py
@@ -60,9 +60,13 @@ def run_circle():
         draw_boy(x, y) 
     pass
 
+def run_triangle():
+    pass
+
 while(1) :
     run_circle()
     run_rectangle()
+    run_triangle()
     # break
     # **TOP-DOWN-DESIGN : 큰 틀을 잡고 내부를 채우는 하향식 설계 방식**
 
